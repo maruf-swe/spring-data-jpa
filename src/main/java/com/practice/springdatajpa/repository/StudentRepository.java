573d25519248faf2f205c9b88c094ec5a2bb4843
Maruf <marufur1847@gmail.com>
2024-01-27 23:54:38 +0600

    studentrepository

---
+++ src/main/java/com/practice/springdatajpa/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.practice.springdatajpa.repository;
+
+import com.practice.springdatajpa.entity.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
