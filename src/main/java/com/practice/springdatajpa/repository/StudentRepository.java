408299f1dd342b5803164e49700318ab1ca1b466
Maruf <marufur1847@gmail.com>
2024-01-30 23:59:41 +0600

    Modifying and transactional

--- src/main/java/com/practice/springdatajpa/repository/StudentRepository.java
+++ src/main/java/com/practice/springdatajpa/repository/StudentRepository.java
@@ -1,7 +1,9 @@
 package com.practice.springdatajpa.repository;
 
 import com.practice.springdatajpa.entity.Student;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -42,4 +44,13 @@ public interface StudentRepository extends JpaRepository<Student, Long> {
             nativeQuery = true
     )
     Student getStudentByEmailAddressNativeNamedParam(@Param("emailId") String emailId);
+
+    @Modifying
+    @Transactional
+    @Query(
+            value = "update student_table set first_name = ?1 where email_address =?2",
+            nativeQuery = true
+    )
+    int updateStudentNamebyEmailId(String firstName, String emailId);
+
 }
