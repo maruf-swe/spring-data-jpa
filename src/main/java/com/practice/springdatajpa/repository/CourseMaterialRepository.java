c02595beaf2b85b6faeecfffdc7b2c3483cc75bb
Maruf <marufur1847@gmail.com>
2024-01-31 23:59:21 +0600

    repository

---
+++ src/main/java/com/practice/springdatajpa/repository/CourseMaterialRepository.java
@@ -0,0 +1,9 @@
+package com.practice.springdatajpa.repository;
+
+import com.practice.springdatajpa.entity.CourseMaterial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseMaterialRepository extends JpaRepository<CourseMaterial, Long> {
+}
---
+++ src/main/java/com/practice/springdatajpa/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.practice.springdatajpa.repository;
+
+import com.practice.springdatajpa.entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
