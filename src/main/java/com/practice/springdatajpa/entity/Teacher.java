8aa9bc37e5151ae09f82807d3c40d68c4c31c4c1
Maruf <marufur1847@gmail.com>
2024-02-03 23:52:06 +0600

    add techer entity

--- src/main/java/com/practice/springdatajpa/entity/Teacher.java
+++ src/main/java/com/practice/springdatajpa/entity/Teacher.java
@@ -9,23 +9,20 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Course {
+public class Teacher {
+
     @Id
     @SequenceGenerator(
-            name = "course_sequence",
-            sequenceName = "course_sequence",
+            name = "teacher_sequence",
+            sequenceName = "teacher_sequence",
             allocationSize = 1
     )
     @GeneratedValue(
             strategy = GenerationType.SEQUENCE,
-            generator = "course_sequence"
+            generator = "teacher_sequence"
     )
-    private Long courseId;
-    private String title;
-    private Integer credit;
+    private Long teacherId;
+    private String firstName;
+    private String lastName;
 
-    @OneToOne(
-            mappedBy = "course"
-    )
-    private CourseMaterial courseMaterial;
 }
